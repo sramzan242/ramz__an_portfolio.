83187d948aae66660c1eb42a67d140f9a0c4f63d
Ramzan Sheikh <114577983+sramzan242@users.noreply.github.com>
2023-06-23 10:10:44 +0530

    first commit

---
+++ about.js
@@ -0,0 +1,441 @@
+
+/*
+Note: 
+- this is my first canvas piece and first interaction with es6.
+- if there are ways of improving this i would love you to reach out and let me know. 
+*/
+
+
+// create funMouse function 
+function funMouse() {
+  
+  
+  
+  
+    // get canvas
+    const canvas = document.querySelector(".js--canvas");
+    const canvasContext = canvas.getContext("2d");
+  
+    
+    
+    
+    // set canvas size
+    let canvasWidth = (canvas.width = window.innerWidth);
+    let canvasHeight = (canvas.height = window.innerHeight);
+  
+    
+    
+    
+    // get mouse position
+    let mouseX = canvasWidth / 2;
+    let mouseY = canvasHeight / 2;
+  
+    
+    
+    
+    // create circles
+    let circle = {
+      radius: 12,
+      lastX: mouseX,
+      lastY: mouseY
+    };
+  
+    let miniCircle = {
+      radius: 3,
+      lastX: mouseX,
+      lastY: mouseY
+    };
+  
+    
+    
+    
+    // get all data-hover elements
+    // obvi this is better one a web page with more than one anchor tag
+    const elements = [...document.querySelectorAll("a")];
+  
+    
+    
+    
+    // resize canvas function
+    var resizeCanvas = function resizeCanvas() {
+      canvasWidth = canvas.width = window.innerWidth;
+      canvasHeight = canvas.height = window.innerHeight;
+    };
+  
+    
+    
+    
+    // create var holding mouseRender function
+    var mouseRender = function mouseRender() {
+      
+      
+      // clear canvas so no colours or styles overlap
+      canvasContext.clearRect(0, 0, canvasWidth, canvasHeight);
+  
+      
+      // get circles x-coordinate and y-coordinate based on mouse coordinates
+      // the small circle has a slight delay due to the last parms passed
+      circle.lastX = lerp(circle.lastX, mouseX, 0.5);
+      circle.lastY = lerp(circle.lastY, mouseY, 0.5);
+      
+      miniCircle.lastX = lerp(miniCircle.lastX, mouseX, 0.1);
+      miniCircle.lastY = lerp(miniCircle.lastY, mouseY, 0.1);
+  
+      
+      // create first circle
+      canvasContext.beginPath();
+      canvasContext.arc(
+        circle.lastX,
+        circle.lastY,
+        circle.radius,
+        0,
+        Math.PI * 2,
+        false
+      );
+      canvasContext.lineWidth = 2;
+      canvasContext.strokeStyle = "white";
+      canvasContext.stroke();
+      canvasContext.closePath();
+  
+      
+      // create small/second circle
+      canvasContext.beginPath();
+      canvasContext.arc(
+        miniCircle.lastX,
+        miniCircle.lastY,
+        miniCircle.radius,
+        0,
+        Math.PI * 2,
+        false
+      );
+      canvasContext.fillStyle = "white";
+      canvasContext.fill();
+      canvasContext.closePath();
+  
+      
+      // render/draw mouse by calling requestAnimationFrame() and passing itself through
+      requestAnimationFrame(mouseRender);
+    };
+  
+    
+    
+    
+    // mouseInit function
+    var mouseInit = function mouseInit() {
+      
+      
+      // render/draw mouse by calling requestAnimationFrame() and passing mouseRender
+      requestAnimationFrame(mouseRender);
+  
+      
+      // on mouse move update coordinates
+      window.addEventListener("mousemove", function (e) {
+        mouseX = e.clientX;
+        mouseY = e.clientY;
+      });
+  
+      
+      // update canvas size on window resize
+      window.addEventListener("resize", resizeCanvas, false);
+  
+      
+      // style mouse on hover function
+      function on() {
+        canvasContext.clearRect(0, 0, canvasWidth, canvasHeight);
+        canvasContext.beginPath();
+        canvasContext.arc(
+          circle.lastX,
+          circle.lastY,
+          circle.radius,
+          0,
+          Math.PI * 2,
+          false
+        );
+        canvasContext.lineWidth = 3;
+        canvasContext.strokeStyle = "#46C1F6";
+        canvasContext.stroke();
+        canvasContext.closePath();
+  
+        requestAnimationFrame(on);
+      }
+  
+      
+      // style mouse off hover
+      // should be the same are prior to any hover
+      // i noticed if i don't do this then the on styles will stay even after hover
+      function off() {
+        canvasContext.clearRect(0, 0, canvasWidth, canvasHeight);
+        canvasContext.beginPath();
+        canvasContext.arc(
+          circle.lastX,
+          circle.lastY,
+          circle.radius,
+          0,
+          Math.PI * 2,
+          false
+        );
+        canvasContext.lineWidth = 2;
+        canvasContext.strokeStyle = "white";
+        canvasContext.stroke();
+        canvasContext.closePath();
+  
+        canvasContext.beginPath();
+        canvasContext.arc(
+          miniCircle.lastX,
+          miniCircle.lastY,
+          miniCircle.radius,
+          0,
+          Math.PI * 2,
+          false
+        );
+        canvasContext.fillStyle = "white";
+        canvasContext.fill();
+        canvasContext.closePath();
+  
+        requestAnimationFrame(off);
+      }
+  
+      
+      // using TweenMax.min.js by GSAP addin easing for radius for circle
+      // https://cdnjs.cloudflare.com/ajax/libs/gsap/1.20.3/TweenMax.min.js
+      let tween = TweenMax.to(circle, 0.25, {
+        radius: circle.radius * 2.5,
+        ease: Power1.easeInOut,
+        paused: true
+      });
+  
+      
+      // for each hover element add mouseenter and mouseleave actions
+      elements.forEach((el) => {
+        el.addEventListener( "mouseenter", () => { on(); }, false );
+        el.addEventListener( "mouseleave", () => { off(); }, false );
+        el.addEventListener( "mouseenter", () => { tween.play(); }, false );
+        el.addEventListener( "mouseleave", () => { tween.reverse(); }, false );
+      });
+    };
+  
+    
+    // tbh this is from ReGGae's pen that is added in the html comment at the top
+    // from my understanding it does a bunch of math to figure out a smooth position update, including the delay on the mini circle
+    // my [very poor] translation of this math:
+    // shape.lastX = lerp(shape.lastX, mouseX, delay);
+    // above === below
+    // return (1 - delay) * shape.lastX + delay * mouseX;
+    var lerp = function lerp(a, b, n) {
+      return (1 - n) * a + n * b;
+    };
+  
+    
+    //init the mouse function
+    mouseInit();
+  }
+  
+  
+  // run fun mouse to have this cursor
+  funMouse();
+
+  
+$(document).ready(function(){
+    $(window).scroll(function(){
+        // sticky navbar on scroll script
+        if(this.scrollY > 20){
+            $('.navbar').addClass("sticky");
+        }else{
+            $('.navbar').removeClass("sticky");
+        }
+        
+        // scroll-up button show/hide script
+        if(this.scrollY > 500){
+            $('.scroll-up-btn').addClass("show");
+        }else{
+            $('.scroll-up-btn').removeClass("show");
+        }
+    });
+
+    // slide-up script
+    $('.scroll-up-btn').click(function(){
+        $('html').animate({scrollTop: 0});
+        // removing smooth scroll on slide-up button click
+        $('html').css("scrollBehavior", "auto");
+    });
+
+    $('.navbar .menu li a').click(function(){
+        // applying again smooth scroll on menu items click
+        $('html').css("scrollBehavior", "smooth");
+    });
+
+    // toggle menu/navbar script
+
+const hamburger = document.querySelector(".hamburger");
+const navLinks = document.querySelector(".nav-links");
+const links = document.querySelectorAll(".nav-links li");
+
+hamburger.addEventListener('click', ()=>{
+   //Animate Links
+    navLinks.classList.toggle("open");
+    links.forEach(link => {
+        link.classList.toggle("fade");
+
+    });
+
+    //Hamburger Animation
+    hamburger.classList.toggle("toggle");
+});
+    // $('.menu-btn').click(function(){
+    //     $('.navbar .menu').toggleClass("active");
+    //     $('.menu-btn i').toggleClass("deactive");
+    // });
+
+    // typing text animation script
+    var typed = new Typed(".typing", {
+        strings: ["Web Developer", "Hackathon Enthusiast", "Front End Developer", "UI Designer", "Photographer", "Technical Things Learner", "Cloud Computing Enthusiast"],
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop: true
+    });
+
+    var typed = new Typed(".typing-2", {
+        strings: ["Web Developer", "Hackathon Enthusiast", "Front End Developer", "UI Designer", "Photographer", "Technical Things Learner", "Cloud Computing Enthusiast"],
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop: true
+    });
+
+});
+
+
+function animateHomePage(){
+  var tl = gsap.timeline();
+  tl
+  .from("nav",{
+    y:150,
+    opacity:0,
+    // delay:4.5,
+    // stagger:.05,
+    ease:Expo.easeInOut
+})
+  .from(".home-content .text-2",{
+      y:100,
+      opacity:0,
+      duration:1,
+      ease:Expo.easeInOut
+  })
+  .from(".home-content #line",{
+    y:100,
+    opacity:0,
+    duration:.6,
+    delay: -0.5,
+    ease:Expo.easeInOut
+})
+  .from(".home-content .text-1",{
+      y:100,
+      opacity:0,
+      duration:.6,
+      // delay: -0.5,
+      ease:Expo.easeInOut
+  })
+  .from(".home-content .text-3",{
+      y:100,
+      opacity:0,
+      duration:.6,
+      // delay: -0.5,
+      ease:Expo.easeInOut
+  })
+  .from(".about .title",{
+      y:100,
+      opacity:0,
+      duration:.6,
+      // delay: -0.5,
+      ease:Expo.easeInOut
+  })
+
+
+}
+animateHomePage();
+
+gsap.from("#image",{
+  scrollTrigger:{
+      scroller:"body",
+      trigger:".about",
+      start:"top 40%",
+      end:"top 60%",
+      scrub:4,
+      pin:true,
+  //  markers:true
+  },
+  opacity:0,
+  x:-500,
+  duration:1,
+  // stagger:.1,
+  ease:Expo.easeInOut
+});
+gsap.from(".about .text",{
+  scrollTrigger:{
+      scroller:"body",
+      trigger:".about",
+      start:"center 40%",
+      end:"center 50%",
+      scrub:4,
+      pin:true,
+  //  markers:true
+  },
+  opacity:0,
+  x:-600,
+  ease:Expo.easeInOut
+});
+gsap.from(".about p",{
+  scrollTrigger:{
+      scroller:"body",
+      trigger:".about",
+      start:"center 40%",
+      end:"center 50%",
+      scrub:4,
+      pin:true,
+  //  markers:true
+  },
+  opacity:0,
+  x:-600,
+  ease:Expo.easeInOut
+});
+gsap.from(".contact .title2",{
+  scrollTrigger:{
+      scroller:"body",
+      trigger:".contact",
+      start:"top 80%",
+      end:"top 90%",
+      scrub:4,
+      pin:true,
+  //  markers:true
+  },
+  opacity:0,
+  x:-600,
+  ease:Expo.easeInOut
+});
+gsap.from(".contact .wrapper",{
+  scrollTrigger:{
+      scroller:"body",
+      trigger:".contact",
+      start:"top 60%",
+      end:"top 70%",
+      scrub:4,
+      pin:true,
+  //  markers:true
+  },
+  opacity:0,
+  y:500,
+  ease:Expo.easeInOut
+});
+gsap.from(".contact .right button",{
+  scrollTrigger:{
+      scroller:"body",
+      trigger:".contact",
+      start:"top 30%",
+      end:"top 40%",
+      scrub:4,
+      pin:true,
+  //  markers:true
+  },
+  opacity:0,
+  x:-500,
+  ease:Expo.easeInOut
+});
---
+++ script.js
@@ -0,0 +1,636 @@
+
+/*
+Note: 
+- this is my first canvas piece and first interaction with es6.
+- if there are ways of improving this i would love you to reach out and let me know. 
+*/
+
+
+// create funMouse function 
+function funMouse() {
+  
+  
+  
+  
+    // get canvas
+    const canvas = document.querySelector(".js--canvas");
+    const canvasContext = canvas.getContext("2d");
+  
+    
+    
+    
+    // set canvas size
+    let canvasWidth = (canvas.width = window.innerWidth);
+    let canvasHeight = (canvas.height = window.innerHeight);
+  
+    
+    
+    
+    // get mouse position
+    let mouseX = canvasWidth / 2;
+    let mouseY = canvasHeight / 2;
+  
+    
+    
+    
+    // create circles
+    let circle = {
+      radius: 12,
+      lastX: mouseX,
+      lastY: mouseY
+    };
+  
+    let miniCircle = {
+      radius: 3,
+      lastX: mouseX,
+      lastY: mouseY
+    };
+  
+    
+    
+    
+    // get all data-hover elements
+    // obvi this is better one a web page with more than one anchor tag
+    const elements = [...document.querySelectorAll("a")];
+  
+    
+    
+    
+    // resize canvas function
+    var resizeCanvas = function resizeCanvas() {
+      canvasWidth = canvas.width = window.innerWidth;
+      canvasHeight = canvas.height = window.innerHeight;
+    };
+  
+    
+    
+    
+    // create var holding mouseRender function
+    var mouseRender = function mouseRender() {
+      
+      
+      // clear canvas so no colours or styles overlap
+      canvasContext.clearRect(0, 0, canvasWidth, canvasHeight);
+  
+      
+      // get circles x-coordinate and y-coordinate based on mouse coordinates
+      // the small circle has a slight delay due to the last parms passed
+      circle.lastX = lerp(circle.lastX, mouseX, 0.5);
+      circle.lastY = lerp(circle.lastY, mouseY, 0.5);
+      
+      miniCircle.lastX = lerp(miniCircle.lastX, mouseX, 0.1);
+      miniCircle.lastY = lerp(miniCircle.lastY, mouseY, 0.1);
+  
+      
+      // create first circle
+      canvasContext.beginPath();
+      canvasContext.arc(
+        circle.lastX,
+        circle.lastY,
+        circle.radius,
+        0,
+        Math.PI * 2,
+        false
+      );
+      canvasContext.lineWidth = 2;
+      canvasContext.strokeStyle = "white";
+      canvasContext.stroke();
+      canvasContext.closePath();
+  
+      
+      // create small/second circle
+      canvasContext.beginPath();
+      canvasContext.arc(
+        miniCircle.lastX,
+        miniCircle.lastY,
+        miniCircle.radius,
+        0,
+        Math.PI * 2,
+        false
+      );
+      canvasContext.fillStyle = "white";
+      canvasContext.fill();
+      canvasContext.closePath();
+  
+      
+      // render/draw mouse by calling requestAnimationFrame() and passing itself through
+      requestAnimationFrame(mouseRender);
+    };
+  
+    
+    
+    
+    // mouseInit function
+    var mouseInit = function mouseInit() {
+      
+      
+      // render/draw mouse by calling requestAnimationFrame() and passing mouseRender
+      requestAnimationFrame(mouseRender);
+  
+      
+      // on mouse move update coordinates
+      window.addEventListener("mousemove", function (e) {
+        mouseX = e.clientX;
+        mouseY = e.clientY;
+      });
+  
+      
+      // update canvas size on window resize
+      window.addEventListener("resize", resizeCanvas, false);
+  
+      
+      // style mouse on hover function
+      function on() {
+        canvasContext.clearRect(0, 0, canvasWidth, canvasHeight);
+        canvasContext.beginPath();
+        canvasContext.arc(
+          circle.lastX,
+          circle.lastY,
+          circle.radius,
+          0,
+          Math.PI * 2,
+          false
+        );
+        canvasContext.lineWidth = 3;
+        canvasContext.strokeStyle = "#46C1F6";
+        canvasContext.stroke();
+        canvasContext.closePath();
+  
+        requestAnimationFrame(on);
+      }
+  
+      
+      // style mouse off hover
+      // should be the same are prior to any hover
+      // i noticed if i don't do this then the on styles will stay even after hover
+      function off() {
+        canvasContext.clearRect(0, 0, canvasWidth, canvasHeight);
+        canvasContext.beginPath();
+        canvasContext.arc(
+          circle.lastX,
+          circle.lastY,
+          circle.radius,
+          0,
+          Math.PI * 2,
+          false
+        );
+        canvasContext.lineWidth = 2;
+        canvasContext.strokeStyle = "white";
+        canvasContext.stroke();
+        canvasContext.closePath();
+  
+        canvasContext.beginPath();
+        canvasContext.arc(
+          miniCircle.lastX,
+          miniCircle.lastY,
+          miniCircle.radius,
+          0,
+          Math.PI * 2,
+          false
+        );
+        canvasContext.fillStyle = "white";
+        canvasContext.fill();
+        canvasContext.closePath();
+  
+        requestAnimationFrame(off);
+      }
+  
+      
+      // using TweenMax.min.js by GSAP addin easing for radius for circle
+      // https://cdnjs.cloudflare.com/ajax/libs/gsap/1.20.3/TweenMax.min.js
+      let tween = TweenMax.to(circle, 0.25, {
+        radius: circle.radius * 2.5,
+        ease: Power1.easeInOut,
+        paused: true
+      });
+  
+      
+      // for each hover element add mouseenter and mouseleave actions
+      elements.forEach((el) => {
+        el.addEventListener( "mouseenter", () => { on(); }, false );
+        el.addEventListener( "mouseleave", () => { off(); }, false );
+        el.addEventListener( "mouseenter", () => { tween.play(); }, false );
+        el.addEventListener( "mouseleave", () => { tween.reverse(); }, false );
+      });
+    };
+  
+    
+    // tbh this is from ReGGae's pen that is added in the html comment at the top
+    // from my understanding it does a bunch of math to figure out a smooth position update, including the delay on the mini circle
+    // my [very poor] translation of this math:
+    // shape.lastX = lerp(shape.lastX, mouseX, delay);
+    // above === below
+    // return (1 - delay) * shape.lastX + delay * mouseX;
+    var lerp = function lerp(a, b, n) {
+      return (1 - n) * a + n * b;
+    };
+  
+    
+    //init the mouse function
+    mouseInit();
+  }
+  
+  
+  // run fun mouse to have this cursor
+  funMouse();
+
+const loader = ".loader", // without jquery symbols $() because animejs not work with jquery selectors :(
+  loaderCounter = $(".loader__container__counter"),
+  loaderLayer = $(".loader__layer");
+
+// Timer
+let counter = 0;
+const loaderTimer = setInterval(function () {
+  counter++;
+  const tl = anime.timeline();
+  tl.add({
+    targets: ".loader__layer",
+    duration: 0,
+    width: counter + "%"
+  });
+  if (counter == 100) {
+    clearInterval(loaderTimer);
+    tl.add({
+      targets: [".loader", ".loader__layer", ".loader__container"],
+      duration: 700,
+      visibility: "hidden",
+      translateY: "-100%",
+      easing: "easeInExpo",
+      delay: 300,
+      onComplete:function(){
+        animateHomePage();
+    }
+    });
+  }
+  loaderCounter.html(counter + "%");
+  // loaderLayer.css({width: counter + "%"});
+}, 35);
+
+
+$(document).ready(function(){
+    $(window).scroll(function(){
+        // sticky navbar on scroll script
+        if(this.scrollY > 20){
+            $('.navbar').addClass("sticky");
+        }else{
+            $('.navbar').removeClass("sticky");
+        }
+        
+        // scroll-up button show/hide script
+        if(this.scrollY > 500){
+            $('.scroll-up-btn').addClass("show");
+        }else{
+            $('.scroll-up-btn').removeClass("show");
+        }
+    });
+
+    // slide-up script
+    $('.scroll-up-btn').click(function(){
+        $('html').animate({scrollTop: 0});
+        // removing smooth scroll on slide-up button click
+        $('html').css("scrollBehavior", "auto");
+    });
+
+    $('.navbar .menu li a').click(function(){
+        // applying again smooth scroll on menu items click
+        $('html').css("scrollBehavior", "smooth");
+    });
+
+    // toggle menu/navbar script
+
+const hamburger = document.querySelector(".hamburger");
+const navLinks = document.querySelector(".nav-links");
+const links = document.querySelectorAll(".nav-links li");
+
+hamburger.addEventListener('click', ()=>{
+   //Animate Links
+    navLinks.classList.toggle("open");
+    links.forEach(link => {
+        link.classList.toggle("fade");
+
+    });
+
+    //Hamburger Animation
+    hamburger.classList.toggle("toggle");
+});
+
+    var typed = new Typed(".typing", {
+        strings: ["Web Developer", "Hackathon Enthusiast", "Front End Developer", "UI Designer", "Photography", "Technical Things Learner", "Cloud Computing Enthusiast"],
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop: true
+    });
+
+    var typed = new Typed(".typing-2", {
+        strings: ["Web Developer", "Hackathon Enthusiast", "Front End Developer", "UI Designer", "Photography", "Technical Things Learner", "Cloud Computing Enthusiast"],
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop: true
+    });
+
+
+  });
+  
+
+  function animateHomePage(){
+    var tl = gsap.timeline();
+    tl
+    .from("nav",{
+      y:150,
+      opacity:0,
+      // delay:4.5,
+      // stagger:.05,
+      ease:Expo.easeInOut
+  })
+    .from(".home-content .text-1",{
+        y:100,
+        opacity:0,
+        duration:1,
+        delay: 4.5,
+        ease:Expo.easeInOut
+    })
+    .from(".home-content .text-2",{
+        y:100,
+        opacity:0,
+        duration:1,
+        delay: -0.5,
+        ease:Expo.easeInOut
+    })
+    .from(".home-content .text-3",{
+        y:100,
+        opacity:0,
+        duration:1,
+        delay: -0.5,
+        ease:Expo.easeInOut
+    })
+    .from(".home-content .row",{
+        y:100,
+        opacity:0,
+        duration:1,
+        delay: -0.5,
+        ease:Expo.easeInOut
+    })
+    .from(".home-content .hire",{
+        y:100,
+        opacity:0,
+        duration:.8,
+        delay: -0.5,
+        ease:Expo.easeInOut
+    })
+
+  }
+  animateHomePage();
+
+  gsap.from(".about h1",{
+    scrollTrigger:{
+        scroller:"body",
+        trigger:".home",
+        start:"center 30%",
+        end:"center 10%",
+        scrub:4,
+        pin:true,
+    //  markers:true
+    },
+    opacity:0,
+    y:100,
+    duration:1,
+    stagger:.1,
+    ease:Expo.easeInOut
+  });
+  gsap.from(".about-content .left",{
+    scrollTrigger:{
+        scroller:"body",
+        trigger:".home",
+        start:"center 20%",
+        end:"center 10%",
+        scrub:4,
+        pin:true,
+    //  markers:true
+    },
+    opacity:0,
+    x:-600,
+    duration:5,
+    ease:Expo.easeInOut
+  });
+  gsap.from(".about-content .right",{
+    scrollTrigger:{
+        scroller:"body",
+        trigger:".about",
+        start:"center 70%",
+        end:"center 80%",
+        scrub:4,
+        pin:true,
+    //  markers:true
+    },
+    opacity:0,
+    x:-600,
+    duration:1,
+    ease:Expo.easeInOut
+  });
+  gsap.from(".teams .title",{
+    scrollTrigger:{
+        scroller:"body",
+        trigger:".teams",
+        start:"top 60%",
+        end:"top 80%",
+        scrub:2,
+        pin:true,
+    //  markers:true
+    },
+    opacity:0,
+    x: -500,
+    duration:1,
+    stagger:.1,
+    ease: "easeinOut"
+  });
+  gsap.from(".teams  h3",{
+    scrollTrigger:{
+        scroller:"body",
+        trigger:".teams",
+        start:"top 60%",
+        end:"top 80%",
+        scrub:2,
+        pin:true,
+    //  markers:true
+    },
+    opacity:0,
+    x: -500,
+    duration:1,
+    stagger:.1,
+    ease: "easeinOut"
+  });
+  gsap.from(".elem-div .image",{
+    scrollTrigger:{
+        scroller:"body",
+        trigger:".teams",
+        start:"top 20%",
+        end:"top 60%",
+        scrub:4,
+        pin:true,
+    //  markers:true
+    },
+    opacity:0,
+    y: 500,
+    duration:4,
+    // delay:5,
+    stagger:.5,
+    ease: Expo.easeinOut
+  });
+
+  gsap.from(".achievement .title",{
+    scrollTrigger:{
+        scroller:"body",
+        trigger:".achievement",
+        start:"top 80%",
+        end:"top 90%",
+        scrub:4,
+        pin:true,
+    //  markers:true
+    },
+    opacity:0,
+    x: -500,
+    duration:4,
+    // delay:5,
+    stagger:.5,
+    ease: Expo.easeinOut
+  });
+  gsap.from(".achievement h3",{
+    scrollTrigger:{
+        scroller:"body",
+        trigger:".achievement",
+        start:"top 80%",
+        end:"top 90%",
+        scrub:4,
+        pin:true,
+    //  markers:true
+    },
+    opacity:0,
+    x: -500,
+    duration:4,
+    // delay:5,
+    stagger:.5,
+    ease: Expo.easeinOut
+  });
+  gsap.from(".achievement .left",{
+    scrollTrigger:{
+        scroller:"body",
+        trigger:".achievement",
+        start:"top 60%",
+        end:"top 80%",
+        scrub:4,
+        pin:true,
+    //  markers:true
+    },
+    opacity:0,
+    x: -500,
+    duration:4,
+    // delay:5,
+    stagger:.5,
+    ease: Expo.easeinOut
+  });
+  gsap.from(".achievement .right",{
+    scrollTrigger:{
+        scroller:"body",
+        trigger:".achievement",
+        start:"top 25%",
+        end:"top 50%",
+        scrub:4,
+        pin:true,
+    //  markers:true
+    },
+    opacity:0,
+    x: -500,
+    duration:4,
+    // delay:5,
+    stagger:.5,
+    ease: Expo.easeinOut
+  });
+
+
+
+
+
+  gsap.from(".contact .title",{
+    scrollTrigger:{
+        scroller:"body",
+        trigger:".contact",
+        start:"top 60%",
+        end:"top 80%",
+        scrub:4,
+        pin:true,
+    //  markers:true
+    },
+    opacity:0,
+    x: -500,
+    duration:1,
+    // delay:5,
+    // stagger:.5,
+    ease: Expo.easeinOut
+  });
+
+  gsap.from(".contact h3",{
+    scrollTrigger:{
+        scroller:"body",
+        trigger:".contact",
+        start:"top 50%",
+        end:"top 70%",
+        scrub:4,
+        pin:true,
+    //  markers:true
+    },
+    opacity:0,
+    x: -500,
+    duration:1,
+    // delay:5,
+    // stagger:.5,
+    ease: Expo.easeinOut
+  });
+  gsap.from(".wrapper ",{
+    scrollTrigger:{
+        scroller:"body",
+        trigger:".contact",
+        start:"top 30%",
+        end:"top 50%",
+        scrub:4,
+        pin:true,
+    //  markers:true
+    },
+    opacity:0,
+    y: 500,
+    duration:1,
+    // delay:.5,
+    stagger:.5,
+    ease: Expo.easeinOut
+  });
+  gsap.from(".contact .right",{
+    scrollTrigger:{
+        scroller:"body",
+        trigger:".contact",
+        start:"top 10%",
+        end:"top 30%",
+        scrub:4,
+        pin:true,
+    //  markers:true
+    },
+    opacity:0,
+    x: -500,
+    duration:1,
+    // delay:5,
+    // stagger:.5,
+    ease: Expo.easeinOut
+  });
+//   document.querySelectorAll(".home-content")
+// .forEach(function(elem){
+//   gsap.from(elem, {
+//     // scrollTrigger:{
+//     //     trigger:elem,
+//     //     start:"30% 95%",
+//     //     markers: true,
+//     //   },
+//     y:50,
+//     opacity:0,
+//     duration:2,
+//     delay:4.5,
+//     ease:Expo.easeInOut
+// })
+// })
